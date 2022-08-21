463f6232eb678d7c75a45a8703fcbab2fd28ee23
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 15:44:01 +0200

    Added better strings git guts

--- src/test.java
+++ src/test.java
@@ -1,6 +1,14 @@
 public class test {
+
+    private static String type = "String Blocks";
+    private static String variables = "variables in the format;";
+
     public static void main(String[] args) {
         System.out.println("hallo world");
-        System.out.println("this is from mortn");
+
+        System.out.println("""
+                Remember to use %s
+                and use all the proper %s
+                """.formatted(type, variables));
     }
 }
