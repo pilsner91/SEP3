a2e23a24a159c1e5dfc53e90f8b1e596bd7d78de
toften05 <jacob.toft2001@gmail.com>
2022-11-02 10:18:08 +0100

    mindre opdatering i dto

--- Domain/DTOs/WareCreationDto.cs
+++ Domain/DTOs/WareCreationDto.cs
@@ -2,9 +2,9 @@ namespace Shared.DTOs;
 
 public class WareCreationDto {
 
-    private string KundeNummer { get; }
-    private string VareNummer { get; }
-    private string VareAntal { get; }
+    public string KundeNummer { get; }
+    public string VareNummer { get; }
+    public string VareAntal { get; }
     
     public WareCreationDto(string kundeNummer, string vareNummer, string vareAntal)
     {
