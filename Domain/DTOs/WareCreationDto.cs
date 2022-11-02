9357fd32bcfc3d62d491e150d32a1964330e5bab
toften05 <jacob.toft2001@gmail.com>
2022-11-02 10:12:00 +0100

    updated CreateWare.razor

--- Domain/DTOs/WareCreationDto.cs
+++ Domain/DTOs/WareCreationDto.cs
@@ -1,5 +1,17 @@
 namespace Shared.DTOs; 
 
 public class WareCreationDto {
+
+    private string KundeNummer { get; }
+    private string VareNummer { get; }
+    private string VareAntal { get; }
     
+    public WareCreationDto(string kundeNummer, string vareNummer, string vareAntal)
+    {
+        KundeNummer = kundeNummer;
+        VareNummer = vareNummer;
+        VareAntal = vareAntal;
+
+    }
 }
+
