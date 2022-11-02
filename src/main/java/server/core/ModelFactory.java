c590165fb5d5b6adae0ee9b80c28bf47ef1e04ab
RinaDeithe <316455@via.dk>
2022-11-02 08:29:13 +0100

    Java + C# opdateret til Tech demo

---
+++ src/main/java/server/core/ModelFactory.java
@@ -0,0 +1,11 @@
+package server.core;
+
+public enum ModelFactory {
+    Instance;
+
+    public String gitGut() {
+        return "Better git gut";
+    }
+
+
+}
---
+++ src/test/java/server/database/DatabaseTest.java
@@ -0,0 +1,11 @@
+package server.database;
+
+import org.junit.jupiter.api.Test;
+import server.core.ModelFactory;
+
+class DatabaseTest {
+    @Test
+    void thisIsATestTest() {
+        ModelFactory.Instance.gitGut();
+    }
+}
