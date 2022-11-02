5bc3ce888b12d63b95741aae2aaa578d0838d082
RinaDeithe <316455@via.dk>
2022-11-02 20:04:36 +0100

    - Slettede GRPCTestClient

--- Logic/Logic/ItemLogic.cs
+++ Logic/Logic/ItemLogic.cs
@@ -1,6 +1,3 @@
-using System.Text.Json;
-using GRPCClient;
-using GrpcService1;
 using Logic.LogicInterfaces;
 using Shared.DTOs;
 using Shared.Model;
@@ -9,15 +6,6 @@ namespace Logic.Logic;
 
 public class ItemLogic : IItemLogic {
     public async Task<Item> CreateAsync(ItemCreationDto dto) {
-
-        HelloRequest request = new HelloRequest();
-
-        request.Name = "Test";
-        
-        Task<HelloReply> reply = EtellerAndet.PingServerAsync(request);
-
-        Item ware = new Item();
-
-        return ware;
+        throw new NotImplementedException();
     }
 }
