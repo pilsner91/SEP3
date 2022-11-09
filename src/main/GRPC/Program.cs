3a69c8cbd24dc78629bbf16ac2ead785ea97abff
morte <morten_oel@hotmail.com>
2022-11-09 09:24:50 +0100

    it doesn't work :(

--- src/main/GRPC/Program.cs
+++ src/main/GRPC/Program.cs
@@ -1,19 +1,7 @@
-using GRPC.Services;
+using System.Threading.Tasks;
+using GRPC;
+using Grpc.Net.Client;
+using GRPCGreeterClient;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
+using var channel = GrpcChannel.ForAddress("https://localhost:9090");
+var Client = new Greeter.GRPC(channel);
