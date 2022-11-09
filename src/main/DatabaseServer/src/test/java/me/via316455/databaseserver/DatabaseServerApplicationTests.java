4f7c3ccbf94e7193f7fc24054a31e2562b1956e5
morte <morten_oel@hotmail.com>
2022-11-09 09:25:49 +0100

    unversioned

---
+++ src/main/DatabaseServer/src/test/java/me/via316455/databaseserver/DatabaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.via316455.databaseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/GRPCClient/GRPCClient/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServer;
+
+var channel = GrpcChannel.ForAddress("https://localhost:9090");
